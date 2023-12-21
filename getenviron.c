1b1e1c988737a147fe5429bf1250f05d665c47e7
azzaguesmi00 <azzaguesmi77@gmail.com>
2023-12-21 15:27:41 +0100

    description

--- getenviron.c
+++ getenviron.c
@@ -3,24 +3,24 @@
  * _getenv- returns the value of env
  * @var: *char
  * Return: *char
-*/
+ */
 char *_getenv(char *var)
 {
-    char *tmp, *value, *key, *env;
-    int i;
+	char *tmp, *value, *key, *env;
+	int i;
 
-    for (i = 0; environ[i]; i++)
-    {
-        tmp = strdup(environ[i]);
-        key = strtok(tmp, "=");
-        if (key != NULL && strcmp(key, var) == 0)
-        {
-            value = strtok(NULL, "\n");
-            env = (value != NULL) ? strdup(value) : NULL;
-                free(tmp), tmp = NULL;
-            return (env);
-        }
-        free(tmp), tmp = NULL;
-    }
-    return (NULL);
+	for (i = 0; environ[i]; i++)
+	{
+		tmp = strdup(environ[i]);
+		key = strtok(tmp, "=");
+		if (key != NULL && strcmp(key, var) == 0)
+		{
+			value = strtok(NULL, "\n");
+			env = (value != NULL) ? strdup(value) : NULL;
+			free(tmp), tmp = NULL;
+			return (env);
+		}
+		free(tmp), tmp = NULL;
+	}
+	return (NULL);
 }
